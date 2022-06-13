b4a8f7dda00ac7e2f1efbaeb92f3300218120dd5
nothking <nothking@qq.com>
2022-06-13 16:28:05 +0800

    重拳出击

---
+++ test/e1051_test.go
@@ -0,0 +1,37 @@
+package test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test1051(t *testing.T) {
+	fmt.Println(heightChecker([]int{1, 1, 4, 2, 1, 3}))
+}
+
+func heightChecker(heights []int) int {
+	lex := len(heights)
+	target := make([]int, lex)
+	copy(target, heights)
+
+	var temp int
+
+	for i := range target {
+		for j := i + 1; j < lex; j++ {
+			if target[i] > target[j] {
+				temp = target[j]
+				target[j] = target[i]
+				target[i] = temp
+			}
+		}
+	}
+	res := 0
+	fmt.Println(target)
+	fmt.Println(heights)
+	for i := range target {
+		if target[i] != heights[i] {
+			res++
+		}
+	}
+	return res
+}
