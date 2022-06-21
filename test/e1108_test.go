01bf77ea66173fad3e48a791a0be6107d55d2e30
nothking <nothking@qq.com>
2022-06-21 19:18:52 +0800

    easy

---
+++ test/e1108_test.go
@@ -0,0 +1,19 @@
+package test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func Test1108(t *testing.T) {
+	fmt.Println(defangIPaddr("1.1.1.1"))
+}
+
+func defangIPaddr(address string) string {
+	return strings.Join(strings.Split(address, "."), "[.]")
+}
+
+func defangIPaddr1(address string) string {
+	return strings.ReplaceAll(address, ".", "[.]")
+}
