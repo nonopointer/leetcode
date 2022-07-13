2a61d13412e26eb9ff1c1b43bd0214dcce3b3c25
nothking <nothking@qq.com>
2022-07-13 20:49:44 +0800

    true

--- test/m735_test.go
+++ test/m735_test.go
@@ -1,57 +1,18 @@
 package test
 
-import (
-	"fmt"
-	"testing"
-)
-
-func Test735(t *testing.T) {
-	fmt.Println(asteroidCollision([]int{-5, 10, -15}))
-}
-
-func asteroidCollision(asteroids []int) []int {
-	lex := len(asteroids)
-	res := make([]int, lex)
-	res[0] = asteroids[0]
-	idx := 0
-	for i := 1; i < lex; i++ {
-		fmt.Println("res : ", res[:idx+1])
-		testsss(res, asteroids, &idx, i)
-	}
-	return res[:idx+1]
-}
-
-func testsss(res, asteroids []int, idx *int, i int) {
-	if res[*idx] < 0 {
-		*idx++
-		res[*idx] = asteroids[i]
-	} else {
-		if confictTwoNum(res[*idx], asteroids[i]) {
-
-			if res[*idx] > -asteroids[i] {
-				return
+func asteroidCollision(asteroids []int) (st []int) {
+	for _, aster := range asteroids {
+		alive := true
+		for alive && aster < 0 && len(st) > 0 && st[len(st)-1] > 0 {
+			alive = st[len(st)-1] < -aster // aster 是否存在
+			if st[len(st)-1] <= -aster {   // 栈顶行星爆炸
+				st = st[:len(st)-1]
 			}
-			if res[*idx] == -asteroids[i] {
-				*idx--
-				return
-			}
-
-			for *idx >= 0 && res[*idx] < -asteroids[i] {
-				res[*idx] = asteroids[i]
-				*idx--
-				if *idx < 0 {
-					*idx = 0
-					break
-				}
-			}
-
-		} else {
-			*idx++
-			res[*idx] = asteroids[i]
+		}
+		if alive {
+			st = append(st, aster)
 		}
 	}
-}
+	return st
 
-func confictTwoNum(a, b int) bool {
-	return (a > 0) != (b > 0)
 }
